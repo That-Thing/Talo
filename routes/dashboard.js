e8aa84c75f2a1a8b188ea13945af30cd66349a55
That-Thing <sen@onee.ch>
2022-12-21 00:39:24 -0500

    Fixed issue with dashboard router naming. Navbar links are now active on the right pages.

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,10 +3,14 @@ var router = express.Router();
 const config = require('../modules/config');
 const errors = require('../modules/errors');
 const connection = require('../modules/connection');
+const url = require("url");
+let path = ""; //Current path. useful for navbar.
 router.use(function(req, res, next) { //Middleware to check if user is logged in
     if (!req.session.loggedIn) {
         return res.redirect('/auth/login');
     }
+    path = url.parse(req.url).path; //Set path to current path
+    console.log(path);
     next();
 });
 
@@ -14,7 +18,7 @@ router.get("/", function(req, res) {
     connection.query(`SELECT * FROM accounts WHERE id = '${req.session.user}'`, function (error, result) { //Get user data
         if (error) throw error;
         var user = result[0];
-        res.render('dashboard', {config: config, user: user});
+        res.render('dashboard', {config: config, user: user, path: path}); //Render dashboard
     });
 });
 
